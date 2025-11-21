48cd0a7f13432c74fbda82fd5d6afb4a008e712c
Vahid Siyami <vahidsiyami.dev@gmail.com>
2025-11-22 00:36:12 +0330

    config.py

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ from dotenv import load_dotenv
 from os import getenv
 
 load_dotenv()
-
-TRAIN_PATH = getenv("TRAIN_PATH")
-TEST_PATH = getenv("TEST_PATH")
-TRAIN_PROCESSED_PATH = ""
-TEST_PROCESSED_PATH = ""
+DATASET_PATH = getenv("DATASET_PATH")
+X_TRAIN_PATH = getenv("X_TRAIN_PATH")
+X_TEST_PATH = getenv("X_TEST_PATH")
+Y_TRAIN_PATH = getenv("Y_TRAIN_PATH")
+Y_TEST_PATH = getenv("Y_TEST_PATH")
