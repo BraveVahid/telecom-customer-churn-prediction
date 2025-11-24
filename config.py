f2c9d9b17e4f6ad9e08e0f2f68fe06b16a0f00a7
Vahid Siyami <vahidsiyami.dev@gmail.com>
2025-11-24 21:19:41 +0330

    Add config.py

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
+import os
 from dotenv import load_dotenv
-from os import getenv
 
 load_dotenv()
-DATASET_PATH = getenv("DATASET_PATH")
-X_TRAIN_PATH = getenv("X_TRAIN_PATH")
-X_TEST_PATH = getenv("X_TEST_PATH")
-Y_TRAIN_PATH = getenv("Y_TRAIN_PATH")
-Y_TEST_PATH = getenv("Y_TEST_PATH")
+
+DATASET_PATH = os.getenv("DATASET_PATH")
+X_TRAIN_PATH = os.getenv("X_TRAIN_PATH")
+X_TEST_PATH = os.getenv("X_TEST_PATH")
+Y_TRAIN_PATH = os.getenv("Y_TRAIN_PATH")
+Y_TEST_PATH = os.getenv("Y_TEST_PATH")
+MODEL_PATH = os.getenv("MODEL_PATH")
